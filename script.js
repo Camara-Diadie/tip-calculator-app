055fe3eec49f3d89e4a10cf70867aa0c9261cd16
Camara-Diadie <camara-diade@hotmail.fr>
2022-03-08 21:39:36 +0100

    correction des id dans le html , correction du script

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
-const facture = document.getElementById("facture");
-const divisPers = document.getElementById("divis-pers");
-const tipsPers = document.getElementById("tips-person");
-const labelPeople = document.getElementById("pers-label");
-let factureBefor = document.getElementById("facture-befor");
-const buttonTips = document.querySelectorAll("button");
+var factureAfter = document.getElementById("facture-after");
+var facture = document.getElementById("facture");
+var divisNbPers = document.getElementById("divis-pers");
+var tipsPers = document.getElementById("tips-person");
+var labelPeople = document.getElementById("pers-label");
+var buttonTips = document.querySelectorAll("div#button-pourcentage > button");
+var customButton = document.getElementById("pourcentage-custom");
+var totalFactPerso = document.getElementById("total-person");
+var divTips = document.getElementById("pourcentage-tips");
 
-console.log("sa marche");
+divisNbPers.addEventListener("click", function () {});
 
-//cette fonction sert a calculer le prix  a payer au total avec les tips par personne
-function calculFacturtotalTips(totalTips, prixTotal, nombrePerson) {
-  return totalTips + prixTotal / nombrePerson;
-}
-/// cette fonction permet de calculer les tips par personne
-function calculTipsPers(prixTotalTips, nombrePerson) {
-  return prixTotalTips / nombrePerson;
-}
-
-// cette focntion permet de calucler le pourcentage de tips a payer par raport a facture
-function CalculPoucentage(prixTotal, pourcentage) {
-  return (prixTotal * pourcentage) / 100;
-}
 for (let i = 0; i < buttonTips.length; i++) {
   buttonTips[i].addEventListener("click", function () {
-    let tip = buttonTips[i].value;
-    factureBefor.addEventListener("input", function () {});
-    const facturePrice = factureBefor.value;
+    var resultTipsTotalParPers =
+      ((parseInt(factureAfter.value) / parseInt(divisNbPers.value)) *
+        parseInt(buttonTips[i].value)) /
+      100;
+      tipsPers.innerHTML = resultTipsTotalParPers;
 
-    tipsPers.innerHTML = "$" + CalculPoucentage(facturePrice, tip);
-    console.log(CalculPoucentage(facturePrice, tip));
+    //   //
+
+    var testecustom = customButton.value;
+    var resultTipsCustomTotalParPers =
+      ((parseInt(factureAfter.value) / parseInt(divisNbPers.value)) *
+        parseInt(testecustom)) /
+      100;
+    //   tipsPers.innerHTML = resultTipsTotalParPers.value;
+
+
+    if (tipsPers.innerHTML === "") {
+      tipsPers.innerHTML = resultTipsTotalParPers;
+    } else {
+      tipsPers.innerHTML = resultTipsCustomTotalParPers;
+    }
+
+    
   });
 }
 
-///
-
 const errorSpan = document.createElement("span");
-divisPers.addEventListener("keyup", function () {
-  if (divisPers.value > 0) {
-    errorSpan.innerHTML = "";
+divisNbPers.addEventListener("keyup", function () {
+  if (divisNbPers.value > 0) {
+    errorSpan.innerHTML = " ";
     labelPeople.appendChild(errorSpan);
-    divisPers.classList.remove("error");
+    divisNbPers.classList.remove("error");
   } else {
     errorSpan.innerHTML = "can't be zero";
     labelPeople.appendChild(errorSpan);
-    divisPers.classList.add("error");
+    divisNbPers.classList.add("error");
   }
 });
